8724b4500df7a37640f8d502b3540b6c4b827553
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-07-12 09:01:44 -0700

    finish phase 1 and 2

--- phase-1-arrays.js
+++ phase-1-arrays.js
@@ -1,1 +1,44 @@
 // Your code here
+
+Array.prototype.uniq = function() {
+    res = [];
+    this.forEach((ele)=>{
+        if(res.includes(ele) === false){
+            res.push(ele);
+        }
+    })
+    return res;
+}
+
+
+// console.log([1,2,2,3,3,3].uniq())
+
+Array.prototype.twoSum = function(){
+    res = []
+    for(let i = 0; i < this.length; i++){
+        for(let j = 0; j < this.length; j++){
+            if(i < j && this[i]+ this[j] === 0){
+                res.push([i, j])
+            }
+        }
+    }
+    return res
+}
+
+// console.log([-1, 0, 2, -2, 1].twoSum())
+
+Array.prototype.transpose = function(){
+    res = [];
+    for(let i = 0; i < this.length; i++){
+        res.push([]);
+    }
+    for(let i = 0; i < this.length; i++){
+        for(let j = 0; j < this.length; j++){
+            res[j].push(this[i][j]);
+        }
+    }
+    return res;
+}
+
+// console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose())
+
--- phase-2-enumerable.js
+++ phase-2-enumerable.js
@@ -1,1 +1,33 @@
 // Your code here
+
+Array.prototype.myEach=function(callback){
+    for(let i = 0; i < this.length; i++){
+        callback(this[i])
+
+    }
+}
+
+Array.prototype["myMap"]=function(callback){
+    res = []
+    this.myEach((ele)=>{
+        res.push(callback(ele))
+    });
+    return res
+}
+
+function double(el){
+    return el * 2
+};
+
+// console.log([1,2,3].myMap(double))
+
+Array.prototype.myReduce = function(callback, initialValue=this[0]){
+    this.myEach((ele)=>{
+        initialValue += callback(ele);
+    })
+    return initialValue;
+}
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+}))
